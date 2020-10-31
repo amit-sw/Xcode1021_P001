32f8a52435b0721585759577544d6f2fa6849f97
Amit Gupta <amit@gprof.com>
2020-10-31 14:24:14 -0700

    Initial Commit

---
+++ Xcode1021_P001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode1021_P001
+//
+//  Created by Amit Gupta on 10/31/20.
+//  Copyright © 2020 AI Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
